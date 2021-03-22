a87944fd1bf6f4fe3756ac1412d63786dd907675
brianthf <blora616@gmail.com>
2021-03-22 12:38:21 -0400

    add SecurityUser

---
+++ src/main/java/com/oa/authserver/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.oa.authserver.service;
+
+import com.oa.authserver.domain.SecurityUser;
+import com.oa.authserver.domain.User;
+import com.oa.authserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    public UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        final User user = repository.findByUsername(s);
+        if (user == null)
+            throw new UsernameNotFoundException("Invalid username or password.");
+
+        return new SecurityUser(user);
+    }
+}
