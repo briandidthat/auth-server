c4f615343db4c2d946092a17524311ddb9a5a3af
brianthf <blora616@gmail.com>
2021-03-22 19:40:53 -0400

    fix postconstruct info

--- src/main/java/com/oa/authserver/service/UserDetailsServiceImpl.java
+++ src/main/java/com/oa/authserver/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.oa.authserver.service;
 
+import com.oa.authserver.domain.Role;
 import com.oa.authserver.domain.SecurityUser;
 import com.oa.authserver.domain.User;
 import com.oa.authserver.repository.UserRepository;
@@ -8,9 +9,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import javax.annotation.PostConstruct;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     public UserRepository repository;
+
+    @PostConstruct
+    private void saveTestUser() {
+        User admin = new User("admin", "$2a$10$nkwXt0IXLF8pgheDphrKfe51nOFE6CUYWGTYFL7TwP7tULOK9YuHO", true);
+        // all roles must be prepended with ROLE
+        Role role = new Role("ROLE_ADMIN");
+        // since there is a many to many relationship, we must add the Role to the set of roles before saving user
+        admin.getRoles().add(role);
+
+        repository.save(admin);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
